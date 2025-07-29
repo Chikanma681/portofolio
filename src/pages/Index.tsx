93dde25c3ef610cb2be709c110977a682f8a790e
Chikanma Chika-Onuegbu <74144477+Chikanma681@users.noreply.github.com>
2025-07-29 17:23:24 -0600

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,7 +10,7 @@ const Index = () => {
         <ProfileHeader />
         <div className="mt-16">
           <ProjectsSection />
-          <ServicesSection />
+{/*           <ServicesSection /> */}
         </div>
       </div>
     </div>
